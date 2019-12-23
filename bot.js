c0c0123579eb76324e71a95a80e9617fd2822849
Talon120 <taloncjones@gmail.com>
2019-12-22 21:40:15 -0800

    Add basic !commands and echo functionality

--- bot.js
+++ bot.js
@@ -1,14 +1,41 @@
 var Discord = require('discord.io');
+var auth = require('./auth.json');
 
 var bot = new Discord.Client({
   token: auth.token,
   autorun: true
 });
 
-bot.on('ready', function() {
+let botName;
+bot.on('ready', function () {
   console.log('%s - %s\n', bot.username, bot.id);
+  botName = bot.username;
 });
 
-bot.on('message', function(user, userID, channelID, message, event) {
+bot.on('message', function (user, userID, channelID, message, event) {
+  if (message.substring(0, 1) == '!') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
 
+    args = args.splice(1);
+    switch (cmd) {
+      case 'save':
+        bot.sendMessage({
+          to: channelID,
+          message: 'Save received'
+        });
+        break;
+      case 'list':
+        bot.sendMessage({
+          to: channelID,
+          message: 'List received'
+        });
+        break;
+    }
+  } else if (user != botName) {
+    bot.sendMessage({
+      to: channelID,
+      message: message.substring(0)
+    });
+  }
 });
